e3812d1557f62acf26eb3f934d6b344b44113358
matsuken01152 <matsuken01152@outlook.com>
2025-08-24 16:30:54 +0900

    [update]read-file

--- part02/03_nodecli/main.js
+++ part02/03_nodecli/main.js
@@ -1,10 +1,15 @@
 import * as util from "node:util";
+import * as fs from "node:fs/promises";
 
-const {
-    positionals
-} = util.parseArgs({
+const {positionals} = util.parseArgs({
     allowPositionals: true
 });
 
 const filePath = positionals[0];
-console.log(filePath);
+
+fs.readFile(filePath, {encoding: "utf8"}).then(file => {
+    console.log(file);
+}).catch(err => {
+    console.error(err.message);
+    process.exit(1)
+});
