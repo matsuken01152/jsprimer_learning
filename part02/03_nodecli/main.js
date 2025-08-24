0e264a985df0dbb5c6e2e9fca015cf1f0a125fbe
matsuken01152 <matsuken01152@outlook.com>
2025-08-24 16:20:18 +0900

    [update]02_argument-parse

--- part02/03_nodecli/main.js
+++ part02/03_nodecli/main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import * as util from "node:util";
+
+const {
+    positionals
+} = util.parseArgs({
+    allowPositionals: true
+});
+
+const filePath = positionals[0];
+console.log(filePath);
